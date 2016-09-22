967089cf7ca88e6dcd658bbe67fa9c963fb2167d
Zoe Bachman <zoe.bachman@gmail.com>
2016-09-21 23:02:44 -0400

    added function

--- recommendations.py
+++ recommendations.py
@@ -2,7 +2,22 @@
 # set of movies
 
 from math import sqrt
+# Returns a distance-based similarity score person1 and person2
 
+def sim_distance(prefs, person1, person2):
+	#Get the list of shared_items
+	si = {} #dictionary
+	for item in prefs[person1]:
+		for item in prefs[person2]:
+			si[item] = 1
+	# if they have no ratings in common, return 0
+	if len(si) == 0: return 0
+
+	#Add up the squares of all the differences
+	sum_of_squares = sum([pow(prefs[person1][item] - prefs[person2][item],2) for item in si])
+
+	return 1/(1+sqrt(sum_of_squares))
+	
 critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
  'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5,
  'The Night Listener': 3.0},
